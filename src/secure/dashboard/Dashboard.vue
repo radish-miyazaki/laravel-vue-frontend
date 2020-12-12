a3c992026568d9b15add43a08897eb6b92457ce1
radish-miyazaki <y.hidaka.kobe@gmail.com>
2020-12-12 20:45:37 +0900

    create DailySales Dashboard

--- src/secure/dashboard/Dashboard.vue
+++ src/secure/dashboard/Dashboard.vue
@@ -1,13 +1,53 @@
 <template>
-  Dashboard
+  <div>
+    <h2>Daily Sales</h2>
+    <div id="chart"></div>
+  </div>
 </template>
 
 <script>
+import { onMounted } from "vue";
+import axios from 'axios';
+import * as c3 from 'c3';
+
 export default {
-name: "Dashboard"
+  name: "Dashboard",
+
+  setup() {
+    onMounted(async () => {
+      const chart = c3.generate({
+        bindto: '#chart',
+        data: {
+          x: 'x',
+          columns: [
+            ['x'],
+            ['Sales'],
+          ],
+          types: {
+            Sales: 'bar'
+          }
+        },
+        axis: {
+          x: {
+            type: 'timeseries',
+            tick: {
+              format: '%Y-%m-%d'
+            }
+          }
+        }
+      });
+
+      const response = await axios.get('chart');
+
+      const records = response.data.data;
+
+      chart.load({
+        columns: [
+          ['x', ...records.map(r => r.date)],
+          ['Sales', ...records.map(r => r.sum)]
+        ]
+      });
+    });
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
